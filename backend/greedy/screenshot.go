5a9c2500f7a0f29a45cc6865e9b0d12d3e829ff3
Rogier Lommers <rlommers@bol.com>
2023-01-14 19:37:11 +0100

    debug

--- backend/greedy/screenshot.go
+++ backend/greedy/screenshot.go
@@ -15,11 +15,14 @@ import (
 func createScreenshot(s string) (string, error) {
 
 	target := fmt.Sprintf("https://screenshot.abstractapi.com/v1/?api_key=%s&url=%s", cfg.Settings.ScreenshotAPIToken, url.QueryEscape(s))
+	logrus.Infof("target: %s", target)
 
 	resp, err := http.Get(target)
 	if err != nil {
 		return "", err
 	}
+
+	logrus.Infof("api response code: %d", resp.StatusCode)
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
