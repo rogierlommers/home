5bfebd1a7bd9df535cf8a82f54dd25726b2e522a
Rogier Lommers <rogier@lommers.org>
2024-12-13 16:01:04 +0100

    Remove webhook static

--- internal/homepage/homepage.go
+++ internal/homepage/homepage.go
@@ -33,7 +33,6 @@ func displayHome(c *gin.Context) {
 		    <em>Everything you need at home...</em>
 			<ul>
 				<li><a href="/api/greedy/rss">greedy rss url</a></li>
-				<li><a href="/api/message_webhook/rss">webhook rss url</a></li>
 			</ul>
 		  </p>
 		</section>
